eec84426b96a63601b15304e180534dd1367b5e5
Vieronika Koliesnikova <koliesnnikova@gmail.com>
2026-03-13 17:16:44 +0200

    недороблене шось

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def main():
-    pass
+from app.io.input import(input_console, input_file, read_pandas)
+from app.io.output import (
+output_console,
+output_in_file
+)
 
-if __name__ == '__main__':
-    main()
+def main():
+    input_console = input_console.input_console()
+    input_file = input_file.input_file("data/input.txt")
+    read_pandas = read_pandas.read_pandas("data/input.csv")
+    output_console = output_console.output_console("data/output.csv")
+
