8058feec3b021e0b5e6967228c0fbdb9bc81984f
Vieronika Koliesnikova <koliesnnikova@gmail.com>
2026-03-16 20:27:40 +0200

    docstrings heheh

--- app/io/input.py
+++ app/io/input.py
@@ -2,15 +2,29 @@ import pandas as pd
 
 
 def input_console():
+    """
+        Read text input from the console.
+        Returns: text entered by the user in the console.
+        """
     txt = input("enter text: ")
     return txt
 
 def input_file(filepath):
+    """
+       Read text from a file using built-in python tools.
+       arguments: filepath (str): path to the file that should be read
+       Returns: content of the file as a string.
+       """
     with open(filepath, "r", encoding="utf-8") as file:
         return file.read()
 
 
 def read_pandas(filepath):
+    """
+      Read file content using pandas.
+      arguments: filepath (str): path to a CSV file.
+      Returns: file content converted to string
+      """
     df = pd.read_csv(filepath)
     return df.to_string()
 
--- app/io/output.py
+++ app/io/output.py
@@ -1,8 +1,13 @@
 def output_console(text):
+    """
+      Print text to the console.
+      Arguments: text that will be displayed in the console.
+      """
     print(text)
 
-
-
 def output_in_file(filepath, text):
+    """
+       Write text to a file using built-in tools.
+       Arguments: filepath (str): path to the file where text will be saved,text (str): text that should be written down into the file"""
     with open(filepath, "w") as f:
         f.write(text)
