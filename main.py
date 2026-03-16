c1cecbc96d1a38bdee9126e1588058ae322ff45a
Vieronika Koliesnikova <koliesnnikova@gmail.com>
2026-03-16 20:22:05 +0200

    updated main

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from app.io.input import(input_console, input_file, read_pandas)
-from app.io.output import (
-output_console,
-output_in_file
-)
+from app.io.input import input_console, input_file, read_pandas
+from app.io.output import output_console, output_in_file
+
 
 def main():
-    input_console = input_console.input_console()
-    input_file = input_file.input_file("data/input.txt")
-    read_pandas = read_pandas.read_pandas("data/input.csv")
-    output_console = output_console.output_console("data/output.csv")
+    console_text = input_console()
+    file_text = input_file("data/input.txt")
+    pandas_text = read_pandas("data/input.csv")
+    output_console(console_text)
+    output_console(file_text)
+    output_console(pandas_text)
 
+    combined_text = f"{console_text}\n{file_text}\n{pandas_text}"
+
+    output_in_file("data/output.txt", combined_text)
+
+
+if __name__ == "__main__":
+    main()
